fc9ae22d7264359d2846fda798ca8c179cf68f46
Dominik Hucka <dominikhucka@googlemail.com>
2023-08-13 15:22:46 +0200

    added Winning Combination

--- script.js
+++ script.js
@@ -1,93 +1,145 @@
-let fields = [ // Felder 1-9 
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-    null,
-]
-
-
+let fields = [
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+  null,
+];
+const WINNING_COMBINATIONS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+  [0, 4, 8], [2, 4, 6], // diagonal
+];
+let currentPlayer = 'circle';
 function init() {
-    render();
+  render();
 }
-
-
 function render() {
-    const contentDiv = document.getElementById('container');
-
-    // Generate table HTML
-    let tableHtml = '<table>';
-    for (let i = 0; i < 3; i++) {
-        tableHtml += '<tr>';
-        for (let j = 0; j < 3; j++) {
-            const index = i * 3 + j;
-            let symbol = '';
-            if (fields[index] === 'circle') {
-                symbol = generateBlinkingCircleSVG();
-            } else if (fields[index] === 'cross') {
-                symbol = generateBlinkingCrossSVG();
-            }
-            tableHtml += `<td>${symbol}</td>`;
-        }
-        tableHtml += '</tr>';
-    }
-    tableHtml += '</table>';
-    contentDiv.innerHTML = tableHtml;
-}
-
-
-function generateBlinkingCircleSVG() {
-    const circleSize = 70;
-    const strokeWidth = 10;
-    const blinkDuration = '1s';
-    const blinkRepeatCount = '3';
-  
-    const svg = `<svg width="${circleSize}" height="${circleSize}" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="${circleSize / 2}" cy="${circleSize / 2}" r="${(circleSize - strokeWidth) / 2}" 
-              stroke="rgb(251, 19, 73)" stroke-width="${strokeWidth}" fill="none">
-        <animate attributeName="opacity" values="1;0;1" dur="${blinkDuration}" repeatCount="${blinkRepeatCount}" />
-      </circle>
-    </svg>`;
-  
-    return svg;
+  const contentDiv = document.getElementById('content');
+  // Generate table HTML
+  let tableHtml = '<table>';
+  for (let i = 0; i < 3; i++) {
+      tableHtml += '<tr>';
+      for (let j = 0; j < 3; j++) {
+          const index = i * 3 + j;
+          let symbol = '';
+          if (fields[index] === 'circle') {
+              symbol = generateCircleSVG();
+          } else if (fields[index] === 'cross') {
+              symbol = generateCrossSVG();
+          }
+          tableHtml += `<td onclick="handleClick(this, ${index})">${symbol}</td>`;
+      }
+      tableHtml += '</tr>';
   }
-  
-  // Füge das generierte SVG-HTML in den DOM ein, beispielsweise in das Element mit der ID 'container'
-  document.getElementById('container').innerHTML = blinkingCircleSVG;
-
-  
-function generateBlinkingCrossSVG() {
-  const crossSize = 70;
-  const strokeWidth = 10;
-  const blinkDuration = '1s';
-  const blinkRepeatCount = '3';
-
-  const svg = `<svg width="${crossSize}" height="${crossSize}" xmlns="http://www.w3.org/2000/svg">
-    <!-- Äußerer Rand -->
-    <rect x="0" y="0" width="${crossSize}" height="${crossSize}" 
-          fill="rgb(251, 19, 73)" />
-    
-    <!-- Innen leerer Bereich -->
-    <line x1="${strokeWidth}" y1="${strokeWidth}" x2="${crossSize - strokeWidth}" y2="${crossSize - strokeWidth}" 
-          stroke="none" stroke-width="${strokeWidth}">
-      <animate attributeName="opacity" values="1;0;1" dur="${blinkDuration}" repeatCount="${blinkRepeatCount}" />
-    </line>
-    
-    <line x1="${crossSize - strokeWidth}" y1="${strokeWidth}" x2="${strokeWidth}" y2="${crossSize - strokeWidth}" 
-          stroke="none" stroke-width="${strokeWidth}">
-      <animate attributeName="opacity" values="1;0;1" dur="${blinkDuration}" repeatCount="${blinkRepeatCount}" />
-    </line>
-  </svg>`;
-
-  return svg;
+  tableHtml += '</table>';
+  // Set table HTML to contentDiv
+  contentDiv.innerHTML = tableHtml;
+}
+function handleClick(cell, index) {
+  if (fields[index] === null) {
+      fields[index] = currentPlayer;
+      cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
+      cell.onclick = null;
+      currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+      if (isGameFinished()) {
+          const winCombination = getWinningCombination();
+          drawWinningLine(winCombination);
+      }
+  }
+}
+function isGameFinished() {
+  return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+function getWinningCombination() {
+  for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+      const [a, b, c] = WINNING_COMBINATIONS[i];
+      if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+          return WINNING_COMBINATIONS[i];
+      }
+  }
+  return null;
 }
 
-// Rufe die Funktion auf, um den SVG-Code für das blinkende Kreuz zu generieren
-// const blinkingCrossSVG = generateBlinkingCrossSVG();
 
-// Füge das generierte SVG-HTML in den DOM ein, beispielsweise in das Element mit der ID 'container'
-// document.getElementById('container').innerHTML = blinkingCrossSVG;
+function generateCircleSVG() {
+  let color = '#ddeef6';
+  let width = 70;
+  let height = 70;
+
+  return `<svg width="${width}" height="${height}">
+            <circle cx="35" cy="35" r="30" stroke="${color}" stroke-width="15" fill="none">
+              <animate attributeName="stroke-dasharray" from="0 188.5" to="188.5 0" dur="0.2s" fill="freeze" />
+            </circle>
+          </svg>`;
+}
+
+
+function generateCrossSVG() {
+  let color = '#ff0000';
+  let width = 70;
+  let height = 70;
+
+  let svgHtml = `
+    <svg width="${width}" height="${height}">
+      <line x1="0" y1="0" x2="${width}" y2="${height}"
+        stroke="${color}" stroke-width="15">
+        <animate attributeName="x2" values="0; ${width}" dur="200ms" />
+        <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+      </line>
+      <line x1="${width}" y1="0" x2="0" y2="${height}"
+        stroke="${color}" stroke-width="15">
+        <animate attributeName="x2" values="${width}; 0" dur="200ms" />
+        <animate attributeName="y2" values="0; ${height}" dur="200ms" />
+      </line>
+    </svg>
+  `;
+
+  return svgHtml;
+}
+
+
+function drawWinningLine(combination) {
+  let lineColor = '#ffffff';
+  let lineWidth = 5;
+
+
+  let startCell = document.querySelectorAll(`td`)[combination[0]];
+  let endCell = document.querySelectorAll(`td`)[combination[2]];
+  let startRect = startCell.getBoundingClientRect();
+  let endRect = endCell.getBoundingClientRect();
+
+
+  let contentRect = document.getElementById('content').getBoundingClientRect();
+
+
+  let lineLength = Math.sqrt(
+      Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+  );
+  let lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+
+  let line = document.createElement('div');
+  line.style.position = 'absolute';
+  line.style.width = `${lineLength}px`;
+  line.style.height = `${lineWidth}px`;
+  line.style.backgroundColor = lineColor;
+  line.style.top = `${ startRect.top + startRect.height / 2 - lineWidth / 2 } px`;
+  line.style.left = `${ startRect.left + startRect.width / 2 } px`;
+  line.style.transform = `rotate(${ lineAngle }rad)`;
+  line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2 - contentRect.top}px`;
+  line.style.left = `${startRect.left + startRect.width / 2 - contentRect.left}px`;
+  line.style.transform = `rotate(${lineAngle}rad)`;
+  line.style.transformOrigin = `top left`;
+  document.getElementById('content').appendChild(line);
+}
+
+
+
+
+
+
